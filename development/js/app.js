4ad99f1c49aa246082b146562c3b1470d9559a59
VladMit1 <mitin1508@gmail.com>
2022-06-02 02:53:39 +0200

    2.1_Widok_główny_aplikacji

--- development/js/app.js
+++ development/js/app.js
@@ -1,1 +1,21 @@
-q
+const barSection = document.querySelectorAll('.main__bar--section');
+const mainSection = document.querySelectorAll('.main--section');
+
+barSection.forEach(item => {
+   item.addEventListener('click', () => {
+      let currentBar = item;
+      let sectionClass = currentBar.getAttribute("data-section");
+      let currentSection = document.querySelector(sectionClass);
+      if (!currentBar.classList.contains('open')) {
+         barSection.forEach(item => {
+            item.classList.remove('open')
+         });
+         mainSection.forEach(item => {
+            item.classList.remove('open')
+         });
+         currentBar.classList.add('open');
+         currentSection.classList.add('open');
+      };
+   });
+});
+document.querySelector('.main__bar--section').click();
