91656c9a55727d6a4c3e970277a88d081c859468
apuczka <apuczka@icloud.com>
2022-06-05 11:28:17 +0200

    add html

--- development/js/app.js
+++ development/js/app.js
@@ -19,3 +19,20 @@ barSection.forEach(item => {
    });
 });
 document.querySelector('.main__bar--section').click();
+
+//exit widget info
+
+const exitButtons = document.querySelector('.fa-square-xmark');
+
+// exitButtons.addEventListener('click', () => {
+//    const widget = document.querySelector('.widget')
+//    widget.style.display ="none";
+// })
+
+exitButtons.forEach(button => {
+   button.addEventListener('click', () => {
+      const widgets = document.querySelector(".widget")
+    return  widgets.style.background = "red";
+   })
+
+})
