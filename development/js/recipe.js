05845bc77723688dbea72e52e567eab1e251f1a3
asset153 <matadamczyk401@gmail.com>
2022-06-06 19:44:19 +0200

    add new func in js file, and refactoring recipe.html

--- development/js/recipe.js
+++ development/js/recipe.js
@@ -1,14 +1,16 @@
 //text area---------------------------------------------------------------
 const textarea = document.querySelectorAll('textarea');
 for (const area of textarea) {
-   area.addEventListener('keydown', resize);
-};
+    area.addEventListener('keydown', resize);
+}
+;
+
 function resize() {
-   const el = this;
-   setTimeout(function () {
-      el.style.cssText = 'height:auto; padding:0';
-      el.style.cssText = 'height:' + el.scrollHeight + 'px';
-   }, 1);
+    const el = this;
+    setTimeout(function () {
+        el.style.cssText = 'height:auto; padding:0';
+        el.style.cssText = 'height:' + el.scrollHeight + 'px';
+    }, 1);
 };
 //------------------------------------------------------------------------
 
@@ -23,45 +25,46 @@ const ingredients = document.querySelector(".main__text--ingredients");
 const btnInstructions = document.querySelector(".main__btn--instruction");
 const btnIngredients = document.querySelector(".main__btn--ingredients");
 
-const olListInstructions = document.querySelector(".footer__title--ol");
-const ulListIngredients = document.querySelector(".footer__title--ul");
-
-const olListItem = document.querySelectorAll(".olListItem");
-const ulListItem = document.querySelectorAll(".ulListItem");
-
 let recipe = {
-   name: "",
-   describe: "",
-   instruction: [],
-   ingredients: [],
+    name: "",
+    describe: "",
+    instruction: [],
+    ingredients: [],
 }
 
 buttonSaveAndClose.addEventListener("click", (e) => {
-   e.preventDefault();
-      recipe = {
-      ...recipe,
-      name: inputName.value,
-      describe: inputDescription.value,
-      instruction: [...olListItem].map(item => item.innerText),
-      ingredients: [...ulListItem].map(item => item.innerText),
-   }
+    const olListItem = document.querySelectorAll(".olListItem");
+    const ulListItem = document.querySelectorAll(".ulListItem");
 
-   //sprawdzenie czy localStorage nie jest puste, dodanie nowego przepisu
-   let allOfRecipes = JSON.parse(localStorage.getItem("recipeData"));
-   if(allOfRecipes === null) allOfRecipes = [];
-   localStorage.setItem('recipeData', JSON.stringify(recipe));
-   allOfRecipes.push(recipe);
-   localStorage.setItem("recipeData", JSON.stringify(allOfRecipes));
+    e.preventDefault();
+    recipe = {
+        ...recipe,
+        name: inputName.value,
+        describe: inputDescription.value,
+        instruction: [...olListItem].map(item => item.innerText),
+        ingredients: [...ulListItem].map(item => item.innerText),
+    }
+
+    //sprawdzenie czy localStorage nie jest puste, dodanie nowego przepisu
+    let allOfRecipes = JSON.parse(localStorage.getItem("recipeData"));
+    if (allOfRecipes === null) allOfRecipes = [];
+    localStorage.setItem('recipeData', JSON.stringify(recipe));
+    allOfRecipes.push(recipe);
+    localStorage.setItem("recipeData", JSON.stringify(allOfRecipes));
+    //
 });
-
+//nasłuchiwanie na przycisk dodania nowej instrukcji
 btnInstructions.addEventListener("click", () => {
-
+    const cloneOlItem = document.querySelectorAll(".olListItem")[0].cloneNode(true);
+    cloneOlItem.querySelector("div > p").innerText = instructions.value;
+    document.querySelector(".footer__title--ol").appendChild(cloneOlItem);
 });
-
+//nasłuchiwanie na przycisk dodania nowego składnika
 btnIngredients.addEventListener("click", () => {
-
+    const cloneUlItem = document.querySelectorAll(".ulListItem")[0].cloneNode(true);
+    cloneUlItem.querySelector("div > p").innerText = ingredients.value;
+    document.querySelector(".footer__title--ul").appendChild(cloneUlItem);
 });
-
 //------------------------------------------------------------------------
 
 
